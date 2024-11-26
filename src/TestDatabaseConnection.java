262a55a1dcf7567d4089df0aab1270a518bfe253
Vorobei Eugene <zhenjvorobei@gmail.com>
2024-11-26 18:22:17 +0300

    something new

--- src/TestDatabaseConnection.java
+++ src/TestDatabaseConnection.java
@@ -9,8 +9,7 @@ import entities.Customer;
 public class TestDatabaseConnection {
     public static void main(String[] args) {
 
-        String url = "jdbc:sqlserver://ALBERT-PC\\mssqlserver01:1433;databaseName=OnlineStore;" +
-                "integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
+        String url = "";
 
 
         try (Connection conn = DriverManager.getConnection(url)) {
